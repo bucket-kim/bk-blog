41506753f42a589dd44d23246044a5274eec95a3
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-04-09 09:14:04 +0900

    post function done, all complete

---
+++ newPost.js
@@ -0,0 +1,21 @@
+const API_URL = "http://localhost:8000/newPost/";
+
+const uploadNewPost = () => {
+  const title = document.getElementById("form-post-title").value;
+  const content = document.getElementById("form-post-content").value;
+  const file = document.getElementById("form-post-image");
+
+  let data = new FormData();
+  data.append("post_image", file.files[0]);
+  data.append("title", title);
+  data.append("content", content);
+
+  fetch(API_URL, {
+    method: "POST",
+    body: data,
+  }).then(() => {
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 1000);
+  });
+};
